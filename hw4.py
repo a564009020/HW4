3ff8be29646d660c0cef18f62a95ae2c84ee6b5b
a564009020 <33393768+a564009020@users.noreply.github.com>
2019-11-12 11:17:11 +0800

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -28,7 +28,7 @@ markColor=(0,255,0)
 frameSum = np.zeros((height,width,3), np.uint8)
 
 # MOG2
-fgbg = cv2.createBackgroundSubtractorMOG2()
+fgbg = cv2.createBackgroundSubtractorMOG2(history = 1000)
 
 # variables
 flag = 0
@@ -73,48 +73,46 @@ while(True):
     if flag > 5:
 
         # 計算共算存5幀的變化量
-        if counter < 5:
+    
             # buttom1 Exit
-            for row in range(20, 200):
-                for col in range(20, 200):
-                    b1 += fgmask[row][col]
-            # buttom2 變換顏色
-            for row in range(20, 200):
-                for col in range(230, 410):
-                    b2 += fgmask[row][col]
-            # buttom3 開啟google
-            for row in range(20, 200):
-                for col in range(440, 620):
-                    b3 += fgmask[row][col]
+        for row in range(20, 200):
+            for col in range(20, 200):
+                b1 += fgmask[row][col]
+        # buttom2 變換顏色
+        for row in range(20, 200):
+            for col in range(230, 410):
+                b2 += fgmask[row][col]
+        # buttom3 開啟google
+        for row in range(20, 200):
+            for col in range(440, 620):
+                b3 += fgmask[row][col]
         
         # 判斷經過5幀框格內的變化量是否超過threshold
-        else:
-            instruct = 0
-            if b1 > 4 * ((180 * 180 * 255) / 3):
-                instruct = 1
-            b1 = 0
-            if b2 > 4 * ((180 * 180 * 255) / 3):
-                if instruct == 0:
-                    instruct = 2
-                else:
-                    instruct = -1
-            b2 = 0
-            if b3 > 4 * ((180 * 180 * 255) / 3):
-                if instruct == 0:
-                    instruct = 3
-                else:
-                    instruct = -1
-            b3 = 0
-            if instruct == 1:
-                break
-            if instruct == 2:
-                changeColor = True
-            if instruct == 3:
-                url = 'https://www.google.com.tw/'
-                webbrowser.open(url)
+        
+        instruct = 0
+        if b1 >((180 * 180 * 255) / 3):
+            instruct = 1
+        b1 = 0
+        if b2 >((180 * 180 * 255) / 3):
+            if instruct == 0:
+                instruct = 2
+            else:
+                instruct = -1
+        b2 = 0
+        if b3 >((180 * 180 * 255) / 3):
+            if instruct == 0:
+                instruct = 3
+            else:
+                instruct = -1
+        b3 = 0
+        if instruct == 1:
+            break
+        if instruct == 2:
+            changeColor = True
+        if instruct == 3:
+            url = 'https://www.google.com.tw/'
+            webbrowser.open(url)
             
-            counter = 0
-        counter += 1
 
     flag += 1
     
